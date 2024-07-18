33ddc0c3cde5e728a032a6b97e2edd6040d1be82
dato201 <146935809+dato201@users.noreply.github.com>
2024-07-18 08:57:31 -0700

    update card dan

--- scripts/card.js
+++ scripts/card.js
@@ -1,15 +1,15 @@
 let card = [
     {
         image: "https://www.shutterstock.com/image-photo/red-wine-bottle-black-blank-260nw-2364803075.jpg",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 1",
         title: "Lorem ipsum dolor sit amet.",
         price: 13.00,
         grade: 5,
         new: true
     },
     {
-        image: "https://png.pngitem.com/pimgs/s/7-78179_wine-bottle-png-image-white-wine-bottle-png.png",
-        name: "Lorem ipsum",
+        image: "https://static.vecteezy.com/system/resources/thumbnails/021/217/339/small_2x/red-wine-bottle-png.png",
+        name: "Lorem ipsum 2",
         title: "Lorem ipsum dolor sit amet.",
         price: 13.00,
         grade: 5,
@@ -19,7 +19,7 @@ let card = [
 
     {
         image: "https://www.pngall.com/wp-content/uploads/15/Wine-Bottle-PNG-Images.png",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 3",
         title: "Lorem ipsum dolor sit amet.",
         price: 7.00,
         grade: 5,
@@ -28,7 +28,7 @@ let card = [
     },
     {
         image: "https://png.pngtree.com/png-vector/20240207/ourlarge/pngtree-red-wine-drink-bottle-png-image_11680582.png",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 4",
         title: "Lorem ipsum dolor sit amet.",
         price: 13.00,
         grade: 5,
@@ -37,7 +37,7 @@ let card = [
 
     {
         image: "https://static.vecteezy.com/system/resources/previews/016/475/594/original/transparent-white-wine-bottle-with-blank-label-and-gold-foil-capsule-seal-png.png",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 5",
         title: "Lorem ipsum dolor sit amet.",
         price: 13.00,
         grade: 5,
@@ -46,7 +46,7 @@ let card = [
 
     {
         image: "https://www.pngall.com/wp-content/uploads/15/Wine-Bottle.png",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 6",
         title: "Lorem ipsum dolor sit amet.",
         price: 13.00,
         grade: 5,
@@ -56,7 +56,7 @@ let card = [
 
     {
         image: "https://e1.pngegg.com/pngimages/87/504/png-clipart-champagne-bottles-gold-and-black-wine-bottle-illustration.png",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 7",
         title: "Lorem ipsum dolor sit amet.",
         price: 20,
         grade: 5,
@@ -66,7 +66,7 @@ let card = [
 
     {
         image: "https://png.pngtree.com/element_our/20200609/ourlarge/pngtree-red-wine-bottle-image_2224353.jpg",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 8",
         title: "Lorem ipsum dolor sit amet.",
         price: 15.20,
         grade: 5,
@@ -76,7 +76,7 @@ let card = [
 
     {
         image: "https://www.shoprite.co.za/medias/10167754EA-checkers515Wx515H?context=bWFzdGVyfGltYWdlc3w4OTM4OXxpbWFnZS9wbmd8aW1hZ2VzL2hiNi9oZGQvOTg1ODc4NTA1MDY1NC5wbmd8ZWFlZDg5NjhmYTI1ZDViNWE3ODJmNGQ0ZGVhOTcyNTAwZGE4ODY1YWNiMTA2NmY2OTA2NThiMTg1N2I4MDRhMg",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 9",
         title: "Lorem ipsum dolor sit amet.",
         price: 13.19,
         grade: 5,
@@ -85,7 +85,7 @@ let card = [
 
     {
         image: "https://www.shoprite.co.za/medias/10163177EA-checkers515Wx515H?context=bWFzdGVyfGltYWdlc3w3NjIzN3xpbWFnZS9wbmd8aW1hZ2VzL2gzZi9oYWQvMTA0MzI3NDQ5MTQ5NzQucG5nfGYyMWIxNTdjNDAxMTgxOThkOGM0NjkyOGRjM2ExOThmMDg0OTUwZmNmNjlhMTViN2FkMjU2MGRjMTBlYjNiNzk",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 10",
         title: "Lorem ipsum dolor sit amet.",
         price: 13.00,
         grade: 5,
@@ -94,7 +94,7 @@ let card = [
 
     {
         image: "https://www.liquortoship.com/cdn/shop/products/NewProject-2023-09-22T020823.053.jpg?v=1695414592",
-        name: "Lorem ipsum",
+        name: "Lorem ipsum 11",
         title: "Lorem ipsum dolor sit amet.",
         price: 13.00,
         grade: 5,
