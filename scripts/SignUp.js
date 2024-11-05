1ca52fdd46c2fd9ff8cb543e5a6c3ab128698ca9
dato201 <146935809+dato201@users.noreply.github.com>
2024-11-05 06:18:28 -0800

    finished Sign Up

--- scripts/SignUp.js
+++ scripts/SignUp.js
@@ -1,17 +1,21 @@
 function SignUp() {
     backgroundLogin.style.display = "flex";
-    backgroundLogin2.style.display = "none";
-    backgroundLogin3.style.display = "none";
+    LogIn1.style.display = "none";
+    SignUp1.style.display = "flex";
+}
+
+function LogIn() {
+    backgroundLogin.style.display = "flex";
+    LogIn1.style.display = "flex";
+    SignUp1.style.display = "none";
 }
 
 let backgroundLogin = document.getElementById("backgroundLogin");
+let SignUp1 = document.getElementById("SignUp");
+let LogIn1 = document.getElementById("LogIn");
 
 window.onclick = function (event) {
     if (event.target == backgroundLogin) {
         backgroundLogin.style.display = "none";
-    } else if ((event.target == backgroundLogin2)) {
-        backgroundLogin2.style.display = "none";
-    } else if ((event.target == backgroundLogin3)) {
-        backgroundLogin3.style.display = "none";
     }
 }
